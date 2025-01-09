dc85456835f7dcc70f2132a4d29559644662991e
DidyMtz <102537566+DidyMtz@users.noreply.github.com>
2025-01-09 11:34:39 +0100

    composant hml

--- sms/src/app/send-sms/send-sms.component.ts
+++ sms/src/app/send-sms/send-sms.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-send-sms',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './send-sms.component.html',
   styleUrl: './send-sms.component.css'
 })
 export class SendSmsComponent {
 
-  
+  smsGroup!: FormGroup
+
+
+  sendSMS(){
+
+  }
+
 }
